75abf2b85a93a23790c6d03511f76ec540f7e878
itdat <intdat@outlook.com>
2021-09-24 22:19:02 +0700

    Add state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
 import React from "react";
 import { AppContainer } from "./styles";
 import { Column } from "./Column";
-import { Card } from "./Card";
 import { AddNewItem } from "./AddNewItem";
+import { useAppState } from "./AppStateContext";
 
-function App() {
+const App = () => {
+  const { state } = useAppState();
   return (
     <AppContainer>
-      <Column text="To Do">
-        <Card text="Generate app scaffold" />
-      </Column>
-      <Column text="In Progress">
-        <Card text="Learn Typescript" />
-      </Column>
-      <Column text="Done">
-        <Card text="Begin to use static typing" />
-      </Column>
+      {state.lists.map((list, i) => (
+        <Column text={list.text} key={list.id} index={i} />
+      ))}
       <AddNewItem
         toggleButtonText="+ Add another list"
         onAdd={console.log}
@@ -23,6 +18,6 @@ function App() {
       />
     </AppContainer>
   );
-}
+};
 
 export default App;
